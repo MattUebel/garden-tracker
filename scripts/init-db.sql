f135e3fba1800e8cd92a0e24cec3ff382e1b6bac
MattUebel <mattuebel@github.com>
2025-03-04 09:49:15 -0500

    reworking

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -4,9 +4,6 @@ CREATE USER garden_user WITH PASSWORD 'mygarden' LOGIN;
 -- Create database if it doesn't exist
 CREATE DATABASE garden_db;
 
--- Grant connection privilege
-GRANT CONNECT ON DATABASE garden_db TO garden_user;
-
 -- Connect to the database
 \c garden_db;
 
@@ -14,15 +11,24 @@ GRANT CONNECT ON DATABASE garden_db TO garden_user;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Grant schema privileges
-GRANT USAGE ON SCHEMA public TO garden_user;
-GRANT CREATE ON SCHEMA public TO garden_user;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES TO garden_user;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON SEQUENCES TO garden_user;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON FUNCTIONS TO garden_user;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT USAGE ON TYPES TO garden_user;
 
--- Grant table privileges
+GRANT ALL ON SCHEMA public TO garden_user;
+GRANT CREATE ON SCHEMA public TO garden_user;
+GRANT USAGE ON SCHEMA public TO garden_user;
+
+-- Grant privileges on existing objects
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO garden_user;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO garden_user;
 GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO garden_user;
 
--- Set default privileges for future objects
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO garden_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO garden_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO garden_user;
+-- Grant database connection privilege
+GRANT CONNECT ON DATABASE garden_db TO garden_user;
+GRANT TEMP ON DATABASE garden_db TO garden_user;
+
+-- Make sure garden_user owns the public schema and has type creation privileges
+ALTER SCHEMA public OWNER TO garden_user;
+GRANT CREATE ON SCHEMA public TO postgres;
